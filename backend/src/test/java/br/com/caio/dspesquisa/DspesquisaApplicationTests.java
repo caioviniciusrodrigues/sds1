11c0329cbbab7613bd9c3e389d9b1061ca8c790d
Caio Rodrigues <rodrigues.caio1985@gmail.com>
2020-09-17 20:57:14 -0300

    Project created

---
+++ backend/src/test/java/br/com/caio/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
